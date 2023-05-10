3f0c0f0244193db65ad2d185719035aab94ff987
Spencer Poisseroux <spoisseroux@me.com>
2023-05-10 11:15:05 -0400

    Working Card Level Prototype, New Star Skyboxes (Can push them to github)

--- Assets/Assets/scripts/UISpriteAnimate.cs
+++ Assets/Assets/scripts/UISpriteAnimate.cs
@@ -5,35 +5,59 @@ using UnityEngine.UI;
 
 public class UISpriteAnimate : MonoBehaviour
 {
+    public string nameForKeepingTrack;
     public Image m_Image;
     public Sprite[] m_SpriteArray;
     public float m_Speed = .02f;
     private int m_IndexSprite;
     Coroutine m_CorotineAnim;
     bool IsDone;
+    public bool playOnAwake = false;
+    public bool triggerNewOnOneShot = false;
+    public UISpriteAnimate spriteAnimToTriggerOnFinish;
+    private bool alreadyPlayed = false;
 
     public void Start()
     {
-        //play anim on start
-        Func_PlayUIAnim();
+        if (playOnAwake) {
+            //play anim on start
+            Func_PlayUIAnim();
+        }
+        
     }
 
     public void Func_PlayUIAnim()
     {
-        IsDone = false;
-        m_CorotineAnim = StartCoroutine(Func_PlayAnimUI());
+        if (alreadyPlayed == false) {
+            IsDone = false;
+            m_CorotineAnim = StartCoroutine(Func_PlayAnimUI());
+        }
     }
     public void Func_StopUIAnim()
     {
         IsDone = true;
         StopCoroutine(m_CorotineAnim);
+
+        if (spriteAnimToTriggerOnFinish != null)
+        {
+            spriteAnimToTriggerOnFinish.Func_PlayUIAnim();
+        }
     }
     IEnumerator Func_PlayAnimUI()
     {
         yield return new WaitForSeconds(m_Speed);
         if (m_IndexSprite >= m_SpriteArray.Length)
         {
-            m_IndexSprite = 0;
+            //if we trigger new sprite anim on finish switch to that
+            if ((triggerNewOnOneShot) && (spriteAnimToTriggerOnFinish != null)) {
+                Func_StopUIAnim();
+            }
+
+            //else loop
+            if (triggerNewOnOneShot == false) {
+                m_IndexSprite = 0;
+            }
+            
         }
         m_Image.sprite = m_SpriteArray[m_IndexSprite];
         m_IndexSprite += 1;
