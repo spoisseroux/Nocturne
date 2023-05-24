7f6248cbc06bc709d770a04bdd80d3dc13562cfa
Spencer Poisseroux <spoisseroux@me.com>
2023-05-24 01:20:06 -0400

    thirdPersonController and animations finished, telescope script

---
+++ Assets/Assets/scripts/TelescopeScript.cs
@@ -0,0 +1,215 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TelescopeScript : MonoBehaviour
+{
+
+    public PlayerMovement playerMovementScript; //optional
+    public PlayerCam playerCamScript; //optional
+
+    public GameObject thirdPersonPlayer; //optional
+
+    public MoveAroundObject moveAroundObjectScript; //optional 
+
+    [SerializeField] GameObject PauseMenu;
+    private bool inScript = false;
+
+    public Action onFinishedShowing;
+
+    public bool spriteExists = false; //optional
+
+    BoxCollider imageCollider; //optional
+    private bool isInCollider = false;
+
+    private GameObject sprite; //optional
+    public AudioSource beginAudio; //optional
+    [HideInInspector] public bool isFinished = false;
+
+    [SerializeField] Image sunSprite; //optional
+    [SerializeField] Image charSprite; //optional
+
+    [SerializeField] GameObject objectToPauseAnim; //optional
+
+    [SerializeField] UIDissolveHandler crossfadeDissolve;
+
+    public bool crossfadeEnter = false;
+    public bool crossfadeExit = false;
+    private bool crossfadeEnterBool = true;
+
+    public Camera mainCamera;
+    public Camera telescopeCamera;
+
+    private void Awake()
+    {
+        imageCollider = GetComponent<BoxCollider>();
+        //anim = crossfadeImage.GetComponent<Animator>();
+
+        //handle question mark sprite
+        if (spriteExists)
+        {
+            sprite = this.transform.GetChild(0).gameObject;
+            sprite.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if ((isInCollider) && (imageCollider))
+        {
+            if (Input.GetKeyUp(KeyCode.E) && (inScript == false) && (PauseMenu.activeSelf == false))
+            {
+                startTelescope(onFinishedShowing);
+            }
+
+        }
+    }
+
+    public void ShowImageOnClick()
+    {
+        if (inScript == false)
+        {
+            startTelescope(onFinishedShowing);
+        }
+    }
+
+    //Start Print
+    public void startTelescope(Action onFinishedShowing)
+    {
+        StartCoroutine(showTelescope(onFinishedShowing));
+    }
+
+    IEnumerator showTelescope( Action onFinishedShowing)
+    {
+        crossfadeEnterBool = true;
+        inScript = true;
+
+        if ((playerCamScript != null) && (playerMovementScript != null))
+        {
+            playerCamScript.isPaused = true; //pause game
+            playerMovementScript.isPaused = true; //pause movement
+        }
+
+        if (moveAroundObjectScript != null)
+        {
+            moveAroundObjectScript.isPaused = true;
+        }
+
+        if (objectToPauseAnim)
+        {
+            objectToPauseAnim.GetComponent<Animator>().speed = 0;
+        }
+
+        if (crossfadeEnter)
+        {
+            yield return StartCoroutine(crossfadeDissolve.StartDissolveIn());
+
+        }
+
+        if (beginAudio != null)
+        {
+            beginAudio.Play(0);
+        }
+
+        //SWICH CAMERAS HERE
+        mainCamera.enabled = false;
+        telescopeCamera.enabled = true;
+
+        if (thirdPersonPlayer)
+        {
+            thirdPersonPlayer.SetActive(false);
+        }
+
+        if (crossfadeEnter)
+        {
+            yield return StartCoroutine(crossfadeDissolve.StartDissolveOut());
+        }
+
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0));
+
+        if (crossfadeExit)
+        {
+            yield return StartCoroutine(crossfadeDissolve.StartDissolveIn());
+        }
+
+        //SWICH CAMERAS HERE
+        telescopeCamera.enabled = false;
+        mainCamera.enabled = true;
+
+        if (thirdPersonPlayer) {
+            thirdPersonPlayer.SetActive(true);
+        }
+        
+
+        if ((playerCamScript != null) && (playerMovementScript != null))
+        {
+            playerCamScript.isPaused = false; //pause game
+            playerMovementScript.isPaused = false; //pause movement
+        }
+
+        if (moveAroundObjectScript != null)
+        {
+            moveAroundObjectScript.isPaused = false;
+        }
+
+        if (objectToPauseAnim)
+        {
+            objectToPauseAnim.GetComponent<Animator>().speed = 1;
+        }
+   
+
+        isFinished = true;
+
+        if (spriteExists)
+        {
+            sprite.SetActive(false);
+        }
+
+        if (crossfadeExit)
+        {
+            yield return StartCoroutine(crossfadeDissolve.StartDissolveOut());
+        }
+
+
+
+
+        inScript = false;
+        onFinishedShowing?.Invoke();
+        StopAllCoroutines(); //NEW
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isInCollider = true;
+
+        if ((charSprite != null) && (sunSprite != null))
+        {
+            //switch to charSprite when in collider
+            sunSprite.enabled = false;
+            charSprite.enabled = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isInCollider = false;
+
+        if ((charSprite != null) && (sunSprite != null))
+        {
+            //switch to charSprite when in collider
+            charSprite.enabled = false;
+            sunSprite.enabled = true;
+        }
+
+    }
+
+    void Start()
+    {
+
+    }
+}
--- Assets/thirdpersonmovement.cs
+++ Assets/thirdpersonmovement.cs
@@ -13,9 +13,19 @@ public class thirdpersonmovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public Animator characterAnimator;
+
+    public AudioSource footstepsSound;
+
+    public float pitchModifier;
+    private float pitch;
+    private float originalPitch;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        originalPitch = footstepsSound.pitch;
+        footstepsSound.enabled = false;
     }
 
     // Update is called once per frame
@@ -27,6 +37,22 @@ public class thirdpersonmovement : MonoBehaviour
 
         if ((direction.magnitude >= 0.1f) || (controller.isGrounded == false))
         {
+            if (controller.isGrounded == true)
+            {
+                //handle walking anim
+                characterAnimator.SetBool("isRunning", true);
+
+                //handle walking sound
+                if (Random.value < 0.5f)
+                    pitch = originalPitch;
+                else
+                    pitch = originalPitch * pitchModifier;
+
+                footstepsSound.pitch = pitch;
+                footstepsSound.enabled = true;
+
+            }
+
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -43,5 +69,13 @@ public class thirdpersonmovement : MonoBehaviour
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
             Debug.Log("Moving");
         }
+        else {
+            //handle walk anim
+            characterAnimator.SetBool("isRunning", false);
+            footstepsSound.enabled = false;
+        }
+
+            
+        
     }
 }
