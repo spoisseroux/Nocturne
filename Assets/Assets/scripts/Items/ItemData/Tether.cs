876d4bd687119d67a1a7acf54d4c6480a7d09748
corioj <corioj@umich.edu>
2023-06-16 16:44:44 -0400

    Re-added items into scene. Fix the flashlight placement on the scarecrow

--- Assets/Assets/scripts/Items/ItemData/Tether.cs
+++ Assets/Assets/scripts/Items/ItemData/Tether.cs
@@ -17,10 +17,10 @@ public class Tether : ItemData
     public override bool Use()
     {
         // move player to spawn location
-        Player.transform.position = translatePlayerTo.position;
-        CameraHolder.transform.position = translatePlayerTo.position;
-        Player.transform.rotation = translatePlayerTo.rotation;
-        CameraHolder.transform.rotation = translatePlayerTo.rotation;
+        //Player.transform.position = translatePlayerTo.position;
+        //CameraHolder.transform.position = translatePlayerTo.position;
+        //Player.transform.rotation = translatePlayerTo.rotation;
+        //CameraHolder.transform.rotation = translatePlayerTo.rotation;
         return true;
     }
 }
